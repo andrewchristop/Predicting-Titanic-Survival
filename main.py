d8635b62782d6cf506ea7b50e1bebcd9b00aa290
Christopher Andrew <christopherandrew@Christophers-MacBook-Pro.local>
2025-05-23 02:53:39 -0400

    Added comments

--- main.py
+++ main.py
@@ -10,23 +10,23 @@ predict = pd.read_csv("./data/test.csv")
 
 #x = features, y= labels
 
-x = train[['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Cabin']]
+x = train[['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Cabin']] #Elimination of useless features
 y = train[['Survived']]
 
-predict_ds = predict[['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Cabin']]
+predict_ds = predict[['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Cabin']] #Columns to be selected by test data
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3) #30% of the training data dedicated for evaluation purposes
 
-train_ds = pd.concat([x_train, y_train], axis=1)
+train_ds = pd.concat([x_train, y_train], axis=1) #Combining labels with features into distinct train-test dataframes
 test_ds = pd.concat([x_test, y_test], axis=1)
 
-train_ds = tfdf.keras.pd_dataframe_to_tf_dataset(train_ds, label='Survived')
+train_ds = tfdf.keras.pd_dataframe_to_tf_dataset(train_ds, label='Survived') #Conversion to TensorFlow datatype
 test_ds = tfdf.keras.pd_dataframe_to_tf_dataset(test_ds, label='Survived')
 predict_ds = tfdf.keras.pd_dataframe_to_tf_dataset(predict_ds)
 
 path = os.listdir('./model')
 
-if(len(path) == 0):
+if(len(path) == 0): #Model is only trained when the model directory is empty
   model_1 = tfdf.keras.RandomForestModel(verbose=2)
   model_1.fit(train_ds)
   model_1.compile(metrics=["accuracy"])
@@ -34,10 +34,10 @@ if(len(path) == 0):
   print()
   model_1.save("./model/")
 
-saved = tf.keras.models.load_model('./model')
+saved = tf.keras.models.load_model('./model') #Loads saved model
 
-results = saved.predict(predict_ds)
-results = np.round(results).astype(int)
-results = pd.DataFrame(results, columns=['Survived'])
-final = pd.concat([predict[['PassengerId']], results], axis=1)
-final.to_csv('./prediction/prediction_result.csv', index=False)
+results = saved.predict(predict_ds) #Runs inference/prediction against the model
+results = np.round(results).astype(int) #Converts output to binary data
+results = pd.DataFrame(results, columns=['Survived']) 
+final = pd.concat([predict[['PassengerId']], results], axis=1) #Displays the results so that PassengerId and Survived columns are side by side
+final.to_csv('./prediction/prediction_result.csv', index=False) #Saves results as CSV
